d4ff20126e7d8bcd48faca056a4d91859ca33d98
Maciej <maciek@testaarosa.pl>
2019-03-13 14:42:24 +0100

    sations detail

---
+++ src/app/shared/detail-row.directive.ts
@@ -0,0 +1,54 @@
+import { Directive, TemplateRef, ViewContainerRef, Input, HostBinding, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[cdkDetailRow]'
+})
+export class DetailRowDirective {
+  private row: any;
+  private tRef: TemplateRef<any>;
+  private opened: boolean;
+  
+  constructor(public vcRef: ViewContainerRef) { }
+
+  @HostBinding('class.expanded')
+  get expended(): boolean {
+    return this.opened;
+  }
+
+  @Input()
+  set cdkDetailRow(value: any) {
+    if (value !== this.row) {
+      this.row = value;
+    }
+  }
+
+  @Input('detailRowTpl')
+  set template(value: TemplateRef<any>) {
+    if (value !== this.tRef) {
+      this.tRef = value;
+    
+    }
+  }
+
+  @HostListener('click')
+  onClick(): void {
+    this.toggle();
+  }
+
+  toggle(): void {
+    if (this.opened) {
+      this.vcRef.clear();
+    } else {
+      this.render();
+    }
+    this.opened = this.vcRef.length > 0;
+  }
+
+  private render(): void {
+    this.vcRef.clear();
+    if (this.tRef && this.row) {
+      this.vcRef.createEmbeddedView(this.tRef, { $implicit: this.row });
+    }
+  }
+
+}
